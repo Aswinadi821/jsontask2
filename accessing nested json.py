7fb69edac0507fb8b400e6706f2a0339aa5eb012
aswin adithiya <53209784+Aswinadi821@users.noreply.github.com>
2021-12-04 00:20:47 +0530

    Update accessing nested json.py

--- accessing nested json.py
+++ accessing nested json.py
@@ -2,17 +2,19 @@ import json
 with open('med12345_20211116_235951.json') as  access_json:
     read_content=json.load(access_json)
 value1=input()
-def getjsonfunc(value1):
-       for key, values in read_content.items():
-        if type(values) is str:
-            if val1==key:
-              yield(values)
-        elif type(values) is dict:
-              for key2, values2 in values.items():
-                if val1 == key2:
-                   yield(values2)
-                elif type(values2) is dict:
-                     for key3, values3 in values2.items():
-                        if val1 == key3:
-                           yield(values3)
-getjsonfunc(value1)
+def getvaluebykey(read_content,val1):
+    for key, value in read_content.items():
+        if type(value) is dict:
+            getvaluebykey(value,val1)
+        elif val1==key:
+            print(value)
+        elif type(value) is type(list()):
+            for val in value:
+                if type(val) is type(str()):
+                    pass
+                elif type(val) is type(list()):
+                    pass
+                else:
+                    getvaluebykey(val,val1)
+getvaluebykey(read_content,val1)
+
