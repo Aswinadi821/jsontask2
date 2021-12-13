1583099623828abc99ec115f5a5fba16c1c7b63f
aswin adithiya <53209784+Aswinadi821@users.noreply.github.com>
2021-12-13 11:04:03 +0530

    Update accessing nested json.py

--- accessing nested json.py
+++ accessing nested json.py
@@ -1,51 +1,53 @@
 import json
-
 with open('med12345_20211116_235951.json') as  access_json:
-    read_content=json.load(access_json)
-val1=input()
-val2=input()
-val3=input()
-def getvaluebykey(read_content,val1):
-    for key, value in read_content.items():
-        if type(value) is dict:
-            getvaluebykey(value,val1)
-        elif val1==key:
-            print(value)
-        elif type(value) is type(list()):
-            for val in value:
-                if type(val) is type(str()):
-                    pass
-                elif type(val) is type(list()):
-                    pass  # use recursive call in this line if the json structure has a nested list  
-                else:
-                    getvaluebykey(val,val1)
+    read_content = json.load(access_json)
+keyinput1 = input()
+keyinput2 = input()
+valueinput2 = input()
+class Json:
+            @staticmethod
+            def getvaluebykey(read_content,inpkeyvalue):
+                for key, value in read_content.items():
+                    if type(value) is dict:
+                        Json.getvaluebykey(value,inpkeyvalue)
+                    elif inpkeyvalue==key:
+                        print(value)
+                    elif type(value) is type(list()):
+                        for val in value:
+                            if type(val) is type(str()):
+                                pass
+                            elif type(val) is type(list()):
+                                pass
+                            else:
+                                Json.getvaluebykey(val,inpkeyvalue)
+            @staticmethod
+            def getvaluebycomponent(read_content,inp2key,inp2value):
 
-def getvaluebycomponent(read_content,val2,val3):
+                for key,value in read_content.items():
+                   try:
+                       if type(value) is dict:
+                          Json.getvaluebycomponent(value,inp2key,inp2value)
 
-    for key,value in read_content.items():
-       try:
-           if type(value) is dict:
-              getvaluebycomponent(value,val2,val3)
+                       elif type(value) is type(list()) and type(value[0]) is dict:
+                            if (value[0][inp2key]) == inp2value:
+                                with open("sample1.json","w") as outfile:
+                                  json.dump(value[0],outfile,indent=2)
 
-           elif type(value) is type(list()) and type(value[0]) is dict:
-                if (value[0][val2]) == val3:
-                    with open("sample1.json","w") as outfile:
-                      json.dump(value[0],outfile,indent=2)
+                            elif (value[1][inp2key]) == inp2value:
+                                with open("sample1.json", "w") as outfile:
+                                    json.dump(value[1], outfile,indent=2)
+                   except IndexError:
+                       print("Data not found in Intercept components")
 
-                elif (value[1][val2]) == val3:
-                    with open("sample1.json", "w") as outfile:
-                        json.dump(value[1], outfile,indent=2)
-       except IndexError:
-           print("Data not found in Intercept components")
-
-           for val in value:
-                if type(val) is type(str()):
-                    pass
-                elif type(val) is type(list()):
-                    pass
-                else:
-                    getvaluebycomponent(val, val2,val3)
-getvaluebykey(read_content,val1)
-getvaluebycomponent(read_content,val2,val3)
+                       for val in value:
+                            if type(val) is type(str()):
+                                pass
+                            elif type(val) is type(list()):
+                                pass
+                            else:
+                                Json.getvaluebycomponent(val, inp2key,inp2value)
+obj = Json()
+print(obj.getvaluebykey(read_content,keyinput1))
+obj.getvaluebycomponent(read_content,keyinput2,valueinput2)
 
 
